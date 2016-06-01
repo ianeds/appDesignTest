39f5c6c9e44bc73f9bc4f76827b9f28cfaada157
Apparent Design <ian@apparentdesign.com>
2016-06-01 10:01:04 +0200

    second
    second

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -20,12 +20,11 @@
             <span class='bold'>Apparent Marketing</span><br>
             Minckelersstraat 18<br>
             6211 GX Maastricht<br>
+            <a href="tel:+310615197033">+31 (0)6 151 97 033</a><br>
             <a class='activePage' href="mailto:info@apparentdesign.com" target="_top">info@apparentdesign.com</a><br>
             <a class='activePage' href="mailto:info@apparentmarketing.com" target="_top">info@apparentmarketing.com</a>
-            
-
-            <span class='bold'>KVK</span> 65483820<br>
-            <span class='bold'>BTW</span> NL366765899B01<br></li>
+            <span class='blocky'><span class='bold'>KVK</span> 65483820</span>
+            <span class='blocky'><span class='bold'>BTW</span> NL366765899B01</span></li>
     </ul>
 </div>
 
