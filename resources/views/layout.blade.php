75ede514f5f585c2f78aa0a7ebab7531a8104dd6
Apparent Design <ian@apparentdesign.com>
2016-06-28 16:25:22 +0200

    updating to lamp

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,17 +2,19 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="description" content="Apparent Design Marketing graphic interactive design">
-        <meta name="keywords" content="graphic design,web,interactive">
-        <meta name="author" content="Apparent Design">
+        <meta name="description" content="graphic design branding interactive web print">
+        <meta name="keywords" content="graphic design,web,branding">
+        <meta name="author" content="Lamp Bureau">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Apparent | Graphic and interactive design studio</title>
+        <title>Lamp Bureau | Graphic design studio</title>
         <link rel="stylesheet" href="/css/normalize.css" type="text/css">
         <link rel="stylesheet" href="/css/app.css" type="text/css">
-        <meta property="og:title" content="Apparent Design" />
+        <script src="https://use.typekit.net/iet7irr.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
+        <meta property="og:title" content="Lamp Bureau" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://apparentdesign.com" />
-        <meta property="og:image" content="http://apparentdesign.com/images/apparent-full.png" />
+        <meta property="og:url" content="http://lampbureau.com" />
+        <meta property="og:image" content="http://lampbureau.com/images/xxx.png" />
         <meta name="p:domain_verify" content="a6f2514211f5379965a9573e5d03419e"/>
     </head>
 <body>
@@ -35,7 +37,7 @@
             <!-- header wordmark -->
                     <div class="headerLogo">
                         <div id="logo">
-                            <a href="/"><img src="/images/apparent-logoB.svg" alt="Apparent Logo" class="h2Logo"></a>
+                            <a href="/"><img src="/images/lamp_logo2.svg" alt="Lamp logo" class="h2Logo"></a>
                         </div>
                     </div>
                 </div>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,14 +4,15 @@
 
 <main id="contactColumn">
     <div class="contactItem primary">
-        <p>Apparent Design is a graphic design agency based in Maastricht, the Netherlands.
+        <p>LAMP is a graphic design agency based in Maastricht, the Netherlands.
         <span class="blocky"><a href="tel:+310615197033">+31 (0)6 151 97 033</a><br>
-        <a href="mailto:info@apparentdesign.com" target="_top" class=''>info@apparentdesign.com</a><br>
+        <a href="mailto:info@lampbureau.com" target="_top" class=''>info@lampbureau.com</a><br>
         </p>
         </div>
     <div class="contactItem secondary">
         <p>
             <span class="noTop">
+            LAMP Design Bureau<br>
             Minckelersstraat 18<br>
             6211 GX Maastricht</span>
             <span class='blocky'><span class='bold'>KVK</span> 65483820<br>
