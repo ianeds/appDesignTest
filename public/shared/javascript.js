b59cf73481bc7a4f9bd8eefc162865e120a78515
Apparent Design <ian@apparentdesign.com>
2016-06-29 18:22:17 +0200

    added js hover code

--- public/shared/javascript.js
+++ public/shared/javascript.js
@@ -72,6 +72,12 @@ $('body').on('click', '.dropnavClosed', function() {
 });
 
 
+$(document).ready(function() {
+    $('.hover').bind('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+    });
+});
 
 /*
 $( "#cross" ).hide();
