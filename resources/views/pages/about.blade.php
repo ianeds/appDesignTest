38836ce979a30783e54c60dc93c784a17f96fe03
Apparent Design <ian@apparentdesign.com>
2016-06-29 13:13:12 +0200

    changes

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,19 +4,30 @@
 
 <main id="contactColumn">
     <div class="contactItem primary">
-        <p>LAMP is a graphic design agency based in Maastricht, the Netherlands.
-        <span class="blocky"><a href="tel:+310615197033">+31 (0)6 151 97 033</a><br>
-        <a href="mailto:info@lampbureau.com" target="_top" class=''>info@lampbureau.com</a><br>
+        <p>Lamp Bureau is a graphic design consultancy that helps businesses, organizations, and people communicate their brand message through expressive and disciplined visual design and concepting.<br><br> Multidisciplinary and concept-driven, Lamp works across digital and print media. Our three main areas of service are:
+        <ul class="servicesList">
+            <li>Web design: websites and other interactive applications
+            </li>
+            <li>Print design: posters, catalogues, publications and other associated collateral</li>
+            <li>Identity and branding: logos and logo systems, style guides, brand concepting</li>
+        </ul> 
+
+        
         </p>
         </div>
     <div class="contactItem secondary">
         <p>
             <span class="noTop">
-            LAMP Bureau<br>
-            Minckelersstraat 18<br>
-            6211 GX Maastricht</span>
-            <span class='blocky'><span class='bold'>KVK</span> 65483820<br>
-            <span class='bold'>BTW</span> NL366765899B01</span>
+                <a href="tel:+310615197033">+31 (0)6 151 97 033</a><br>
+                <a href="mailto:info@lampbureau.com" target="_top" class=''>info@lampbureau.com</a><br><br>
+                Lamp Bureau<br>
+                Minckelersstraat 18<br>
+                6211 GX Maastricht
+            </span>
+            <span class='blocky'>
+                <span class='bold'>KVK</span> 65483820<br>
+                <span class='bold'>BTW</span> NL366765899B01
+            </span>
         </p>
        </div>
 </div>
