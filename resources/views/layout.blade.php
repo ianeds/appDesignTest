9e7a38c808c24ab5498d7a7805c50fb11a18622d
Apparent Design <ian@apparentdesign.com>
2016-06-29 12:27:56 +0200

    typekit shit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,8 +3,17 @@
     <head>
         <meta charset="UTF-8">
         <meta name="description" content="graphic design branding interactive web print">
-        <script src="https://use.typekit.net/iet7irr.js"></script>
-        <script>try{Typekit.load({ async: true });}catch(e){}</script>
+        <script>
+          (function(d) {
+            var config = {
+              kitId: 'iet7irr',
+              scriptTimeout: 3000,
+              async: true
+            },
+            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+          })(document);
+        </script>
+        
         <meta name="keywords" content="graphic design,web,branding">
         <meta name="author" content="Lamp Bureau">
         <meta name="viewport" content="width=device-width, initial-scale=1">
